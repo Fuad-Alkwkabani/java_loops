120804aa29d92c608e8178790da71d1b862ec843
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-11-09 22:37:17 +0100

    fix the variable system

--- src/main/java/java_loops/Number.java
+++ src/main/java/java_loops/Number.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Number{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(system.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Ingresa un numero: ");
         int n =scanner.nextInt();
         printNumber(n);
